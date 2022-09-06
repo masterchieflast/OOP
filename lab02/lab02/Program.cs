1357662b7df7904105f83f16aaaecbe489d374fd
masterchieflast <denis16.05.2004@icloud.com>
2022-09-06 17:46:55 +0300

    lab02

--- lab02/lab02/Program.cs
+++ lab02/lab02/Program.cs
@@ -1,1 +1,33 @@
-﻿
+﻿namespace lab02
+{
+    public class Student
+    {
+        private int id;
+        private string name;
+        private string lastName;
+        private string middleName;
+        private DateTime birthday;
+        private string city;
+        private string phoneNumber;
+        private string faculty;
+        private int course;
+        private int group;
+
+        public int ageСalculation()
+        {
+            var now = DateTime.Today;
+            return now.Year - birthday.Year +
+                   (now.Month > birthday.Month 
+                    || (now.Month == birthday.Month 
+                    && now.Day >= birthday.Day) ? 0 : -1);
+        }
+    }
+
+    public class Program
+    {
+        public static void Main()
+        {
+        }
+
+    }
+}
