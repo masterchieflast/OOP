81528b52d9adff8b58d25c3c1de0a09d8bde03e1
masterchieflast <denis16.05.2004@icloud.com>
2022-10-03 19:01:56 +0300

    lab05

---
+++ laba05/laba04/EnumTypesOfTransformers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba05
+{
+    internal enum EnumTypesOfTransformers
+    {
+        Autobot,
+        Decepticon,
+        Predacon,
+        Dinobot
+    }
+}
---
+++ laba05/laba04/IntelligentBeing.cs
@@ -0,0 +1,22 @@
+﻿namespace laba05
+{
+    public abstract class IntelligentBeing : object
+    {
+        public string Name;
+
+        protected IntelligentBeing(string name, DateTime birthday)
+        {
+            Name = name;
+            _birthday = birthday;
+        }
+
+        private readonly DateTime _birthday;
+        public TimeSpan Age => DateTime.Now.Subtract(_birthday) / 365;
+
+
+        public override string ToString()
+        {
+            return $"this creature:{Name}\t live:{Age} year";
+        }
+    }
+}
