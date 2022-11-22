e4c90568f8b522c49c801b9c537dbb672df0d8d4
masterchieflast <denis16.05.2004@icloud.com>
2022-11-22 16:57:26 +0300

    lab10

---
+++ lab10/lab10/People.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace lab10
+{
+    public class People
+    {
+        public string Name;
+        public string Profession;
+
+        public People(string name, string profession)
+        {
+            Name = name;
+            Profession = profession;
+        }
+
+        public override string ToString()
+        {
+            return $"name: {Name}\tjob: {Profession}\n";
+        }
+    }
+}
---
+++ lab10/lab10/Profession.cs
@@ -0,0 +1,14 @@
+﻿namespace lab10
+{
+    internal class Profession
+    {
+        public string Faculty;
+        public string Job;
+
+        public Profession(string faculty, string job)
+        {
+            Faculty = faculty;
+            Job = job;
+        }
+    }
+}
