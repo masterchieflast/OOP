1827b7ba2b49473e5b95df17577045a238662ccc
masterchieflast <denis16.05.2004@icloud.com>
2022-12-06 21:27:04 +0300

    lab12

---
+++ lab12/lab12/DDDLog.cs
@@ -0,0 +1,39 @@
+﻿namespace lab12
+{
+    public class DDDLog
+    {
+        public static StreamWriter? Logfile;
+        public static void WriteToLog(string action, string fileName = "", string path = "")
+        {
+            using (Logfile = new StreamWriter(@"D:\study\sem3\OOP\lab12\dddlog.txt", true))
+            {
+                var time = DateTime.Now;
+                Logfile.WriteLine("********************************\n");
+                Logfile.WriteLine($"Action: {action}");
+
+                if (fileName.Length != 0)
+                    Logfile.WriteLine($"File Name: {fileName}");
+
+                if (path.Length != 0)
+                    Logfile.WriteLine($"Path: {path}");
+
+                Logfile.WriteLine($"Date/Time: {time.ToLocalTime()}\n");
+            }
+        }
+
+        public static void SearchByDate(DateTime date)
+        {
+            using (var reader = new StringReader(File.ReadAllText(@"D:\study\sem3\OOP\lab12\dddlog.txt")))
+            {
+                var lines = reader.ReadToEnd().Split("********************************\n");
+                foreach (var line in lines)
+                {
+                    if (line.Contains(date.ToString()))
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ lab12/lab12/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace lab12
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            DDDDiskInfo.GetFreeDrivesSpace();
+
+            DDDFileInfo.GetFileInfo(@"D:\study\sem3\OOP\lab12\dddlog.txt");
+            
+            DDDDirInfo.GetDirInfo(@"D:\study\sem3\OOP\lab12\");
+            
+            DDDFileManager.GetAllDirsAndFilesOfDisk(@"D:\");
+            
+            DDDFileManager.GetAllFilesWithExtension(@"D:\study\sem3\ksis\ksis", ".txt");
+            
+            DDDFileManager.CreateZip(@"D:\study\sem3\OOP\lab12\DDDInspect\DDDFiles");
+            
+            DDDLog.SearchByDate(DateTime.Now);
+        }
+    }
+}
